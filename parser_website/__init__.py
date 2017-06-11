1fe303692fb106d13e5e5263bd38ec9aaec3fb4b
Pascal Roessner <pythonmolurus95@gmail.com>
2017-06-11 13:55:01 +0000

    idk

--- parser_website/__init__.py
+++ parser_website/__init__.py
@@ -1,26 +1,36 @@
 
 from flask import Flask, request
 
+import glob
+import os
+from flask import render_template
+
+
 try:
-    from frontend import frontend
     from parser import AprsFiParser
 except ImportError as e:
-    from .frontend import frontend
     from .parser import AprsFiParser
 
 
 def create_app(configfile=None):
     app = Flask(__name__)
-    app.register_blueprint(frontend)
+    aprsparser = AprsFiParser(False)
+    aprsparser.daemon = True
+
+    aprsparser.start()
+
+
     return app
 
 
 app = create_app()
 
+@app.route('/', methods=['GET'])
+def new_school():
+    files = [os.path.basename(x) for x in glob.glob(os.path.join(app.static_folder, 'images')]
+    return render_template('index.html', files=files)
+
+
+
 if __name__ == '__main__':
-
-    aprsparser = AprsFiParser(False)
-    aprsparser.daemon = True
-    aprsparser.start()
-
     app.run(host="0.0.0.0", port=5000, debug=True)
