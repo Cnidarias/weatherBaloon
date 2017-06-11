a31e4dd009c396d7caf55356b55c52a0547ef7b1
Cnidarias <pythonmolurus95@gmail.com>
2017-06-11 15:59:06 +0200

    ...

--- parser_website/__init__.py
+++ parser_website/__init__.py
@@ -14,22 +14,20 @@ except ImportError as e:
 
 def create_app(configfile=None):
     app = Flask(__name__)
+
     aprsparser = AprsFiParser(False)
     aprsparser.daemon = True
-
     aprsparser.start()
-
-
     return app
-
 
 app = create_app()
 
+
 @app.route('/', methods=['GET'])
 def new_school():
-    files = [os.path.basename(x) for x in glob.glob(os.path.join(app.static_folder, 'images')]
+    files = [os.path.basename(x) for x in glob.glob(os.path.join(app.static_folder, 'images', '*.jpg'))]
+    print(files)
     return render_template('index.html', files=files)
-
 
 
 if __name__ == '__main__':
